8f4489c27fd15a10e99f9e9a3d047097eb15aa33
Bradley Remedios <bremedios@gmail.com>
2024-12-12 14:33:05 -0500

    Made use of cmake parallel builds

--- build_and_install.sh
+++ build_and_install.sh
@@ -4,8 +4,10 @@ CWD=`pwd`
 MODULES=("libbpl_sys" "libdevices_dht22" "libbpl_storage" "libbpl_net" "pinode")
 BRANCH=stable
 
+BUILD_UPDATE=0
 BUILD_DEPENDS=0
 BUILD_PROG=0
+BUILD_PROGS=0
 
 usage() {
 	echo "build_and_install.sh <command>"
@@ -14,17 +16,23 @@ usage() {
 	echo "        Builds program"
 	echo "    --depends"
 	echo "        Builds and installs program dependencies"
+	echo "    --update"
+	echo "        Update source packages"
 	echo "    --all"
 	echo "        Builds programs and dependencies"
 }
 
 if [ $# -eq 0 ] ; then
+	BUILD_UPDATE=1	
 	BUILD_PROG=1
 elif [ $# -eq 1 ] ; then
 	if [ "$1" = "--depends" ] ; then
 		BUILD_DEPENDS=1
+	elif [ "$1" == "--update" ] ; then
+		BUILD_UPDATE=1
 	elif [ "$1" = "--all" ] ; then
 		BUILD_DEPENDS=1
+		BUILD_UPDATE=1
 		BUILD_PROG=1
 	else
 		echo "Invalid arguments"
@@ -56,7 +64,7 @@ if [ $BUILD_DEPENDS -ne 0 ] ; then
 	cd $CWD
 fi
 
-if [ $BUILD_PROG -ne 0 ] ; then
+if [ $BUILD_UPDATE -ne 0 ] ; then
 	#
 	#   Clone then build each module
 	#
@@ -71,7 +79,25 @@ if [ $BUILD_PROG -ne 0 ] ; then
 
 		cd $MOD
 		git pull
+	done
+fi
 
+if [ $BUILD_PROG -ne 0 ] ; then
+	echo "cmake -B cmake-build-debug -S ."
+	cmake -B cmake-build-debug -S .
+
+	echo "cmake --build -B cmake-build-debug -j 4"
+	cmake --build cmake-build-debug -j 4
+
+	echo "sudo cmake --install -B cmake-build-debug"
+	sudo cmake --install cmake-build-debug
+fi
+
+if [ $BUILD_PROGS -ne 0 ] ; then
+	#
+	#   Clone then build each module
+	#
+	for MOD in ${MODULES[@]}; do
 		cmake .
 
 		retval=$?
